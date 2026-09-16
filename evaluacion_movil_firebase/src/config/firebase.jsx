249c438f18a83bb7c2403190ec61f1a975e58bbc
MisaelAlexander <alex.riva2022@gmail.com>
2026-09-16 08:51:58 -0600

    add: estructura de carpetas

---
+++ evaluacion_movil_firebase/src/config/firebase.jsx
@@ -0,0 +1,43 @@
+import { initializeApp, FirebaseApp } from "firebase/app";
+import { getFirestore, Firestore } from "firebase/firestore";
+import { getAuth, Auth } from "firebase/auth";
+import {
+  FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID,
+} from '@env';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: FIREBASE_API_KEY ,
+  authDomain: FIREBASE_AUTH_DOMAIN ,
+  projectId: FIREBASE_PROJECT_ID ,
+  storageBucket: FIREBASE_STORAGE_BUCKET ,
+  messagingSenderId: FIREBASE_MESSAGING_SENDER_ID ,
+  appId: FIREBASE_APP_ID ,
+};
+
+console.log("Valor de configuracion", firebaseConfig);
+
+// Initialize Firebase
+const app: FirebaseApp = initializeApp(firebaseConfig);
+if (app) {
+  console.log('Firebase initialized successfully');
+} else {
+  console.log('Firebase initialization failed');
+}
+
+const database: Firestore = getFirestore(app);
+if (database) {
+  console.log('Firestore initialized correctly');
+} else {
+  console.log('Firestore initialization failed');
+}
+
+// Auth (sin persistencia personalizada por ahora; ver nota en el chat)
+const auth: Auth = getAuth(app);
+
+export { database, auth };
